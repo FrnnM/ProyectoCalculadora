61822aed8fde94d82dbdb1e1358fd7725f379369
sisoy <sisoyyo687@gmail.com>
2025-04-08 04:35:04 +0200

    Implementada division

--- Calculadora.java
+++ Calculadora.java
@@ -8,5 +8,12 @@ public class Calculadora {
     public int multiplicar(int a, int b) {
         return a * b;
     }
+    public int dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("División por cero no permitida");
+        }
+    }
 
 }
