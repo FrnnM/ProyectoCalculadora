b51f654daaebaf71d79375a74df94c1beef10504
sisoy <sisoyyo687@gmail.com>
2025-04-08 04:50:08 +0200

    Resta a main

--- Calculadora.java
+++ Calculadora.java
@@ -5,4 +5,14 @@ public class Calculadora {
     public int resta(int a, int b) {
         return a - b;
     }
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+    public int dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("División por cero no permitida");
+        }
+    }
 }
